7f0300976b092d527fbaee3dd43a294b4f4d70fe
Teo-Te <52335032+Teo-Te@users.noreply.github.com>
2024-03-17 12:52:22 +0100

    huaaaai

--- app/fluturime/page.jsx
+++ app/fluturime/page.jsx
@@ -99,7 +99,7 @@ export default function Fluturime() {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center space-y-2 pl-2 w-full">
+                <div className="flex flex-col justify-center space-y-2 p-2">
                     <div className="flex flex-row items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-10 h-10 pr-2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
