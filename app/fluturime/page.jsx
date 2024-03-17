3cbd389e71a0ef9c7c6a260b707f846f0fd512d6
Teo-Te <52335032+Teo-Te@users.noreply.github.com>
2024-03-17 12:49:11 +0100

    haaaaaaaaaaa

--- app/fluturime/page.jsx
+++ app/fluturime/page.jsx
@@ -81,25 +81,25 @@ export default function Fluturime() {
                 </div>
                 }
             </div>
-            <div className="flex flex-row w-full justify-center items-center px-2 sm:px-5 pt-4">
+            <div className="flex flex-row w-full justify-center items-center p-2 sm:px-5 pt-4">
                 <div className="sm:pr-12">
                     <div>
                         <div className="flex flex-col">
-                            <input className="input input-bordered join-item" placeholder="Emri"/>
+                            <input className="input input-bordered join-item w-full" placeholder="Emri"/>
                         </div>
                     </div>
                     <div>
                         <div>
-                            <input className="input input-bordered join-item" placeholder="Mbiemri"/>
+                            <input className="input input-bordered join-item w-full" placeholder="Mbiemri"/>
                         </div>
                     </div>
                     <div>
                         <div>
-                            <input className="input input-bordered join-item" placeholder="Nr. i celularit"/>
+                            <input className="input input-bordered join-item w-full" placeholder="Nr. i celularit"/>
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center space-y-2 pl-2">
+                <div className="flex flex-col justify-center space-y-2 pl-2 w-full">
                     <div className="flex flex-row items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-10 h-10 pr-2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
